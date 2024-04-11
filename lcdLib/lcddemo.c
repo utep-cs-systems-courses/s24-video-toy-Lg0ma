11572a244deac95bb57912629db53b85aba6bfff
Lg0ma <luisg31209@gmail.com>
2024-04-11 11:00:35 -0600

    added square function

--- lcdLib/lcddemo.c
+++ lcdLib/lcddemo.c
@@ -6,6 +6,16 @@
 #include "lcdutils.h"
 #include "lcddraw.h"
 
+void drawSquare(int startCol, int startRow, int width, int height)
+{
+  for(int i = 0; i < width; i++){
+    for(int j = 0; j < height; j++){
+      drawPixel(startCol + i, startRow + j ,COLOR_PINK);
+    }
+  }
+}
+
+
 /** Initializes everything, clears the screen, draws "hello" and a square */
 int
 main()
@@ -16,8 +26,30 @@ main()
 
   clearScreen(COLOR_NAVY);
 
-  drawString5x7(20,20, "im goma", COLOR_GREEN, COLOR_NAVY);
+   drawString5x7(20,60, "Im Goma", COLOR_GREEN, COLOR_NAVY);
 
-  fillRectangle(30,30, 90, 90, COLOR_ORANGE);
+
+  // fillRectangle(30,30, 90, 90, COLOR_ORANGE);
+
+  short centerRow = screenHeight >> 1;
+  short centerCol = screenWidth >> 1;
+
+  int x = 0;
+  int y = 0;
+  int speed = 6;
   
+  for(short i = 0; i < screenWidth; i++){
+      drawPixel(x, centerRow - y, COLOR_PINK);
+      drawPixel(x, centerRow - speed, COLOR_GREEN);
+      x++;
+      y += speed;
+      speed -= 1;
+      if(y <= 0)
+	{
+	  speed = -speed;
+	}
+  }
+  drawSquare(20,20,20,20);
 }
+
+
