d67dd89261147501c47ab7f5154324594a8d62d8
Luis Gomez <luisg31209@gmail.com>
2024-05-02 16:56:29 -0600

    music diff

--- wakedemo/buzzer.c
+++ wakedemo/buzzer.c
@@ -22,8 +22,23 @@ void buzzer_set_period(short cycles)
 
 void playTune(int *notes, int*tempo, int noteAmt)
 {
-  for
+  for(int i = 0; i < noteAmt; i++)
+  {
+    if(i % 2 == 0 ){
+      toggle_red();
+    }else{
+      toggle_green();
+    }
 
+    buzzer_set_period(1000000 / notes[i]);
+    int dur = tempo[i];
+    while(dur--)
+    {
+    __delay_cycles(10000);
+
+    }
+  }
+  buzzer_set_period(bk);
 }
 
 void fur_Elise()
@@ -31,14 +46,10 @@ void fur_Elise()
 
   int notes[] = {E4, Dsharp4, E4, Dsharp4, E4, B3, D4, C4, A3, Asharp3, B3, D3, E3, Gsharp3, A3};
   int tempo[] = {362, 362, 362, 362, 362, 725, 543, 543, 1087, 1087, 543, 1087, 543, 406, 1087};
-  for(int i = 0; i < 22; i++)
-  {
-    buzzer_set_period(notes[i]);
-    __delay_cycles(4500000);
-    buzzer_set_period(0);
-    __delay_cycles(500000);
-  }
-  buzzer_set_period(0);
+
+  int noteAmt = sizeof(notes)/sizeof(notes[i]);
+  playTune(noes,tempo,noteAmt);
+  buzzer_set_period(bk);
 }
 
 void happy_Bday()
@@ -47,42 +58,27 @@ void happy_Bday()
   int notes[] = {G4,G4,A3,G4,C4,B5,bk,G4,G4,A3,G4,D5,C4,bk,G4,G4,G5,E4,C4,B5,A3,bk,F4,F4,E4,C4,D5,C4};
   int tempo[] = {500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,};
 
-  for(int i = 0; i < 28; i++)
-  {
-    buzzer_set_period(notes[i]);
-    __delay_cycles(5000000);
-    buzzer_set_period(0);
-    __delay_cycles(4500000);
-  }
-  buzzer_set_period(0);
+  int noteAmt = sizeof(notes)/sizeof(notes[i]);
+  playTune(noes,tempo,noteAmt);
+  buzzer_set_period(bk);
 }
 
 void Mario()
 {
   int notes[] = {C4,C4,G4,G4,A4,A4,G4,F4,F4,E4,E4,D4,D4,C4,E4,E4,D4,D4,E4,E4,D4,G4,G4,F4,F4,E4,E4,D4,D4,C4};
   int tempo[] = {400,400,400,400,400,400,400,400,400,400,400,400,400,400,400,400,400,400,400,400,400,400,400,400,400,400,400,400,400,400};
-  for(int i = 0; i < 30; i++)
-  {
-    buzzer_set_period(notes[i]);
-    __delay_cycles(2500000);
-    buzzer_set_period(0);
-    __delay_cycles(2500000);
-  }
-  buzzer_set_period(0);
+  
+  int noteAmt = sizeof(notes)/sizeof(notes[i]);
+  playTune(noes,tempo,noteAmt);
+  buzzer_set_period(bk);
 }
 
 void runaway()
 {
   int note[] = {Fsharp4, E4, Dsharp4, Csharp4, B3, A3, Gsharp3, Fsharp3, E3, Dsharp3, Csharp3, B2, A2, Gsharp2, Fsharp2};
   int tempo[] = {680, 680, 680, 680, 680, 680, 680, 680, 680, 680, 680, 680, 680, 680, 680};
-  for(int i = 0; i < 32; i++)
-  {
 
-    buzzer_set_period(notes[i]);
-    __delay_cycles(10000000);
-    buzzer_set_period(0);
-    __delay_cycles(7000000);
-
-  }
-  buzzer_set_period(0);
+  int noteAmt = sizeof(notes)/sizeof(notes[i]);
+  playTune(noes,tempo,noteAmt);
+  buzzer_set_period(bk);
 }
