710703a0c353811b6f9352870bd62ee4882e5452
Luis Gomez <luisg31209@gmail.com>
2024-05-03 10:28:15 -0600

    draw definition

--- wakedemo/main.c
+++ wakedemo/main.c
@@ -1,8 +1,14 @@
 #include <msp430.h>
-#include "libTimer.h"
+#include <stdio.h>
+#include <libTimer.h>
 #include "led.h"
 #include "switches.h"
 #include "buzzer.h"
+#include "lcdutils.h"
+#include "lcddraw.h"
+#include "statemachines.h"
+#include "switches.h"
+#include "draw.h"
 
 int main(void)
 {
@@ -10,6 +16,7 @@ int main(void)
   switch_init(); // switches
   led_init(); // LED
   buzzer_init(); // Buzzer
+  lcd_init();
 
   green_led_on();
   enableWDTInterrupts();
