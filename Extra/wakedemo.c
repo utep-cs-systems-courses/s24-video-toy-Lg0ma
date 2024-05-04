74d1b3d7d9ed75f5a21085aa44a6802f29b3fe82
Luis Gomez <luisg31209@gmail.com>
2024-05-04 17:27:31 -0600

    shine

--- Extra/wakedemo.c
+++ Extra/wakedemo.c
@@ -4,7 +4,7 @@
 #include "lcddraw.h"
 
 
-short drawPos[2] = {1,10}, controlPos[2] = {2, 10};
+short drawPos[2] = {40,20}, controlPos[2] = {41, 20};
 short colVelocity = 1, colLimits[2] = {40, screenWidth/2};
 
 void
